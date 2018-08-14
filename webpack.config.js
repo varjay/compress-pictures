2534d91dbc7f146394265a32cce28f465ca42435
goudan <goudan@baidu.com>
2018-08-14 10:33:03 +0800

    取消babel

--- webpack.config.js
+++ webpack.config.js
@@ -22,11 +22,11 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-      },
+      // {
+      //   test: /\.js$/,
+      //   loader: 'babel-loader',
+      //   exclude: /node_modules/,
+      // },
       {
         test: /\.vue$/,
         use: 'vue-loader'
